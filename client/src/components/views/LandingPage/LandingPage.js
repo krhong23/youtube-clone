373972ea1f8e18440ab7c9c46f222deab741491d
rami <krhong23@gmail.com>
2022-05-19 08:49:02 +0900

    랜딩 페이지에 비디오 목록 출력

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,33 +1,82 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
 import Auth from "../../../hoc/auth";
+import {Avatar, Card, Col, Row, Typography} from "antd";
+import moment from 'moment';
+
+const {Title} = Typography;
+const {Meta} = Card;
 
 function LandingPage() {
-    // useEffect(() => {
-    //     axios.get('/api/hello')
-    //         .then(response => console.log(response.data))
-    // }, [])
 
-    const navigate = useNavigate();
+    const [Videos, setVideos] = useState([])
 
-    const onClickHandler = () => {
-        axios.get('/api/users/logout')
+    // dom이 load되자마자 하는 일
+    useEffect(() => {
+        axios.get('/api/video/getVideos')
             .then(response => {
                 if (response.data.success) {
-                    navigate('/')
+                    console.log(response.data.videos)
+                    setVideos(response.data.videos)
                 } else {
-                    alert('로그아웃하는데 실패하였습니다.')
+                    alert('비디오 가져오기를 실패했습니다.')
                 }
             })
-    }
+    }, [])
+
+    const renderCards = Videos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+        return <Col lg={6} md={8} xs={24}>
+            <div style={{position: 'relative'}}>
+                <img style={{width: '100%'}} src={`http://localhost:5001/${video.thumbnail}`} alt='thumbnail'/>
+                <div className="duration">
+                    <span>{minutes} : {seconds}</span>
+                </div>
+            </div>
+            <br/>
+            <Meta
+                avatar={<Avatar src={video.writer.image}/>}
+                title={video.title}
+                description=""
+            />
+            <span>{video.writer.name}</span><br/>
+            <span
+                style={{marginLeft: '3rem'}}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+        </Col>
+    })
 
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
-            <h2>시작 페이지</h2>
-            <button onClick={onClickHandler}>로그아웃</button>
+        <div style={{width: '85%', margin: '3rem auto'}}>
+            <Title level={2}>Recommended</Title>
+            <hr/>
+            <Row gutter={16}>
+                {renderCards}
+            </Row>
         </div>
-    );
+    )
+
+    // const navigate = useNavigate();
+    //
+    // const onClickHandler = () => {
+    //     axios.get('/api/users/logout')
+    //         .then(response => {
+    //             if (response.data.success) {
+    //                 navigate('/')
+    //             } else {
+    //                 alert('로그아웃하는데 실패하였습니다.')
+    //             }
+    //         })
+    // }
+    // return (
+    //     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+    //         <h2>시작 페이지</h2>
+    //         <button onClick={onClickHandler}>로그아웃</button>
+    //     </div>
+    // );
 }
 
 export default Auth(LandingPage, null);
--- server/routes/video.js
+++ server/routes/video.js
@@ -75,7 +75,7 @@ router.post('/thumbnail', (req, res) => {
 
 router.post('/uploadVideo', (req, res) => {
     // 비디오를 서버에 저장한다.
-
+    // console.log(req.body);
     const video = new Video(req.body)
     video.save((err, doc) => {
         if (err) {
@@ -85,6 +85,15 @@ router.post('/uploadVideo', (req, res) => {
         }
         res.status(200).json({success: true})
     })
+})
+
+router.get('/getVideos', (req, res) => {
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({success: true, videos})
+        })
 })
 
 module.exports = router;
